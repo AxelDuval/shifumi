6ffa5c271ffefd5a834e930fcd53be0ae1e0e785
AxelDuval <axel.duval@gmail.com>
2021-09-17 12:33:44 +0200

    Intégration du système de manches + affichage des scores

--- js/main.js
+++ js/main.js
@@ -6,6 +6,8 @@ var choice = [
     "ciseaux"
 ];
 
+userScore = 0;
+computerScore = 0;
 // ----------------- FUNCTIONS --------------------------
 function randomComputerChoice(choice){
     return choice[Math.floor(Math.random() * choice.length)];
@@ -21,52 +23,76 @@ do {
 // Welcome
 alert("Bienvenue " + userName + ", le jeu va commencer !");
 
+
 // User's choice
 do {
-    var userChoice = prompt("Vous pouvez choisir entre pierre, feuille et ciseaux, saisissez votre choix ci dessous : ").toLowerCase();
-} while(userChoice !== choice[0] && userChoice !== choice[1] && userChoice !== choice[2]);
+    do {
+        var userChoice = prompt("Vous pouvez choisir entre pierre, feuille et ciseaux, saisissez votre choix ci dessous : ").toLowerCase();
+    } while(userChoice !== choice[0] && userChoice !== choice[1] && userChoice !== choice[2]);
 
-// Computer choice
-var computerChoice = randomComputerChoice(choice);
-console.log(computerChoice);
+    // Computer choice
+    var computerChoice = randomComputerChoice(choice);
+    console.log(computerChoice);
 
-// Comparate choices
-if (userChoice === choice[0] && computerChoice === choice[0]) {
-    alert("l'ordinateur a choisi " + computerChoice + ": Egalité !");
+    // Comparate choices + Alert
+    if (userChoice === choice[0] && computerChoice === choice[0]) {
+        alert("l'ordinateur a choisi " + computerChoice + ": Egalité !" + "   ||   " + "Ordinateur : " + computerScore + " | " + userName + " : " + userScore);
+    }
+
+    else if(userChoice === choice[0] && computerChoice ===choice[1]) {
+        computerScore++;
+        alert("l'ordinateur a choisi " + computerChoice + " : Vous avez perdu !" + "   ||   " + "Ordinateur : " + computerScore + " | " + userName + " : " + userScore);
+    }
+
+    else if(userChoice === choice[0] && computerChoice ===choice[2]) {
+        userScore++;
+        alert("l'ordinateur a choisi " + computerChoice + " : Vous avez gagné !" + "   ||   " + "Ordinateur : " + computerScore + " | " + userName + " : " + userScore);
+    }
+
+    else if(userChoice === choice[1] && computerChoice ===choice[1]) {
+        alert("l'ordinateur a choisi " + computerChoice + " : égalité !" + "   ||   " + "Ordinateur : " + computerScore + " | " + userName + " : " + userScore);
+    }
+
+    else if(userChoice === choice[1] && computerChoice === choice[0]) {
+        userScore++;
+        alert("l'ordinateur a choisi " + computerChoice + " : Vous avez gagné !" + "   ||   " + "Ordinateur : " + computerScore + " | " + userName + " : " + userScore);
+    }
+
+    else if(userChoice === choice[1] && computerChoice === choice[2]) {
+        computerScore++;
+        alert("l'ordinateur a choisi " + computerChoice + " : Vous avez perdu !" + "   ||   " + "Ordinateur : " + computerScore + " | " + userName + " : " + userScore);
+    }
+
+    else if(userChoice === choice[2] && computerChoice ===choice[2]) {
+        alert("l'ordinateur a choisi " + computerChoice + " : égalité !" + "   ||   " + "Ordinateur : " + computerScore + " | " + userName + " : " + userScore);
+    }
+
+    else if(userChoice === choice[2] && computerChoice === choice[1]) {
+        userScore++;
+        alert("l'ordinateur a choisi " + computerChoice + " : Vous avez gagné !" + "   ||   " + "Ordinateur : " + computerScore + " | " + userName + " : " + userScore);
+    }
+
+    else if(userChoice === choice[2] && computerChoice === choice[0]) {
+        computerScore++;
+        alert("l'ordinateur a choisi " + computerChoice + " : Vous avez perdu !" + "   ||   " + "Ordinateur : " + computerScore + " | " + userName + " : " + userScore);
+    }
+
+    else  {
+        computerScore++;
+        alert("l'ordinateur a choisi " + computerChoice + " : Vous avez perdu !" + "   ||   " + "Ordinateur : " + computerScore + " | " + userName + " : " + userScore);
+    }
+} while (computerScore !== 3 && userScore !== 3);
+
+if (computerScore === 3) {
+    alert("Ordinateur a gagné cette manche !");
+}
+else {
+    alert(userName + " a gagné cette manche !");
 }
 
-else if(userChoice === choice[0] && computerChoice ===choice[1]) {
-    alert("l'ordinateur a choisi " + computerChoice + " : Vous avez perdu !");
-}
-
-else if(userChoice === choice[0] && computerChoice ===choice[2]) {
-    alert("l'ordinateur a choisi " + computerChoice + " : Vous avez gagné !");
-}
-
-else if(userChoice === choice[1] && computerChoice ===choice[1]) {
-    alert("l'ordinateur a choisi " + computerChoice + " : égalité !");
-}
-
-else if(userChoice === choice[1] && computerChoice === choice[0]) {
-    alert("l'ordinateur a choisi " + computerChoice + " : Vous avez gagné !");
-}
-
-else if(userChoice === choice[1] && computerChoice === choice[2]) {
-    alert("l'ordinateur a choisi " + computerChoice + " : Vous avez perdu !");
-}
-
-else if(userChoice === choice[2] && computerChoice ===choice[2]) {
-    alert("l'ordinateur a choisi " + computerChoice + " : égalité !");
-}
-
-else if(userChoice === choice[2] && computerChoice === choice[1]) {
-    alert("l'ordinateur a choisi " + computerChoice + " : Vous avez gagné !");
-}
-
-else if(userChoice === choice[2] && computerChoice === choice[0]) {
-    alert("l'ordinateur a choisi " + computerChoice + " : Vous avez perdu !");
-}
-
-else  {
-    alert("l'ordinateur a choisi " + computerChoice + " : Vous avez perdu !");
-}
+// Replay
+/* 
+do {
+    var replayChoice = prompt("Rejouer ? tapez oui ou non").toLowerCase();
+} while(userChoice !== "oui" && userChoice !== "non");
+ */
