70f854e9666117839526d7ac2b9c1f066e1f6976
AxelDuval <axel.duval@gmail.com>
2021-09-17 09:45:44 +0200

    Ajout du choix de l'utilisateur (pierre feuille ciseaux)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 // ----------------- VARIABLES ----------------------------
-
+var pierre = "pierre";
+var feuille = "feuille";
+var ciseaux = "ciseaux";
 
 
 // ----------------- FUNCTIONS --------------------------
@@ -15,3 +17,9 @@ do {
 // Welcome
 alert("Bienvenue " + userName + ", le jeu va commencer !");
 
+// User's choice
+
+do {
+    var userChoice = prompt("Vous pouvez choisir entre pierre, feuille et ciseaux, saisissez votre choix ci dessous : ");
+} 
+while(userChoice !== "pierre" && userChoice !== "feuille" && userChoice !== "ciseaux");
