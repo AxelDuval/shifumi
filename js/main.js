98a46bf95011660630894f5654cbfd6f14c9f144
AxelDuval <axel.duval@gmail.com>
2021-09-17 09:05:36 +0200

    Ajout de la fonctionnalité qui permet de récupérer le nom d'utilisateur et lui souhaite la bienvenue

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// ----------------- VARIABLES ----------------------------
+
+
+
+// ----------------- FUNCTIONS --------------------------
+
+
+// ------------------ METHODS ------------------------------
+
+// Recovery of the user name
+do {
+    var userName = prompt("Merci de saisir votre prénom");
+} while (userName == "" || userName == null);
+
+// Welcome
+alert("Bienvenue " + userName + ", le jeu va commencer !");
+
